8106d4d44240698404b74620d0e0a3f2f88e68c4
Ziyu Fu <zfu@sch.org>
2020-10-02 10:54:21 +0800

    Access TWINS personal page
    - Work in progress: can't press download button on the inner frame on webpage

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# TWINS-Calendar-Export
-A Python tool used to automatically get TWINS course registration information and exports a ics calendar file. 
+## TWINS-Calendar-Export
+
+This is a quick Python tool designed for students using TWINS at University of Tsukuba.
+The script takes your UTID logins and automatically gets the course registration information from TWINS. It then compares the course information to the KdB database and exports a ics calendar file for you to import into a calendar app of your own choice. 
+
+#Dependencies
+Python 3
+
+Python dependencies:
+Selenium 
+
+Others:
+chromedriver (https://chromedriver.chromium.org/)
--- TWINSCalendarImport.py
+++ TWINSCalendarImport.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains 
 
 import module_locator
 import time
@@ -33,4 +34,12 @@ WebDriverWait(browswer, 15).until(EC.url_changes(current_url))
 current_url = browswer.current_url
 browswer.find_element_by_xpath("//img[@src='/campusweb/theme/default/newportal/image/icon/note.png']").click()
 WebDriverWait(browswer, 15).until(EC.url_changes(current_url))
-# browswer.find_element_by_xpath("//input[@class='ui-button ui-widget ui-state-default ui-corner-all']")
+browswer.switch_to.frame(browswer.find_element_by_id("main-frame-if"))
+# browser.find_element_by_value(" ダウンロード ").click()
+# browswer.find_element_by_xpath("//input[@type='submit' and @value=' ダウンロード ']").click()
+# browswer.find_element_by_id("rishuReferForm").click()
+time.sleep(5)
+
+ActionChains(browswer).click(browswer.find_element_by_xpath("//input[@type='submit' and @role='button' and @value=' ダウンロード ']")).perform()
+print("success")
+
